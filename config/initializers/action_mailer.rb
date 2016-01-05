72ad527082ba8b6c1443d1a231b23e914f8700f4
Adam Crownoble <adam@obledesign.com>
2016-01-05 11:12:30 +0800

    Configure sending email via SMTP

--- config/initializers/action_mailer.rb
+++ config/initializers/action_mailer.rb
@@ -1,1 +1,5 @@
-Rails.application.config.action_mailer.default_url_options = { host: Settings.app.host }
+Rails.application.config.action_mailer.tap do |cfg|
+  cfg.default_url_options = {host: Settings.app.host}
+
+  cfg.smtp_settings = {address: Settings.email.smtp.server}
+end
