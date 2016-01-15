acbffccd727abae935d78e683e15032d97ef34b7
Adam Crownoble <adam@obledesign.com>
2016-01-15 14:50:37 +0800

    Use RackCAS Mongoid session store
    This allows for single sign out

--- config/initializers/session_store.rb
+++ config/initializers/session_store.rb
@@ -1,3 +1,2 @@
-# Be sure to restart your server when you modify this file.
-
-Rails.application.config.session_store :cookie_store, key: '_gatekeeper_session'
+require 'rack-cas/session_store/rails/mongoid'
+Rails.application.config.session_store :rack_cas_mongoid_store, key: '_gatekeeper_session'
